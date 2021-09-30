65464cf36b545fdf7041a4da45ac7fef34ee2f2f
GM-MT <gmart0402@yandex.kz>
2021-10-01 00:39:29 +0600

    feat: add count-cats, dream-team, what-season and hanoi-tower

--- src/what-season.js
+++ src/what-season.js
@@ -11,7 +11,16 @@ import { NotImplementedError } from '../extensions/index.js';
  * getSeason(new Date(2020, 02, 31)) => 'spring'
  * 
  */
-export default function getSeason(/* date */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function getSeason(date) {
+  // throw new NotImplementedError('Not implemented');
+  
+  if (!date) return 'Unable to determine the time of year!';
+  try {
+    date.toTimeString();
+  } catch (e) {
+    throw new Error('Invalid date!');
+  }
+
+  let month = Math.floor((date.getMonth() + 1) / 3);
+  return month > 3 ? 'winter' : ['winter', 'spring', 'summer', 'fall'][month];
 }
